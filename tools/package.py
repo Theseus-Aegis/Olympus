464c77a176bfe8979088fe335442ed7df2b6d2bf
jonpas <jonpas33@gmail.com>
2016-04-22 20:19:43 +0200

    Add logo, Fix package.py

--- tools/package.py
+++ tools/package.py
@@ -1,12 +1,18 @@
 import os
 import shutil
+import argparse
+
+parser = argparse.ArgumentParser(description="Package mission")
+parser.add_argument("version", type=str, default="1.0.0", help="Specifiy version number")
+args = parser.parse_args()
 
 name = "Olympus"
-version = "1.0.0"
+version = args.version
+print(version)
 ignore_files = [".git", "extras", "release", "tools", ".editorconfig", ".gitignore"]
 release_dir = "release"
-release_dir_copy = "{}/{}_v{}".format(release_dir, name, version)
 
+release_dir_copy = "{}/{}".format(release_dir, name)
 os.chdir("..")
 
 if os.path.exists(release_dir):
@@ -20,8 +26,8 @@ for item in os.listdir(os.path.abspath("")):
     if item not in ignore_files and ".zip" not in item:
         print("Copying {}".format(item))
         if os.path.isdir(item):
-            shutil.copytree(item, release_dir_copy)
+            shutil.copytree(item, "{}/{}".format(release_dir_copy, item))
         else:
             shutil.copy2(item, release_dir_copy)
 
-release_package = shutil.make_archive(release_dir_copy, "zip", release_dir_copy)
+release_package = shutil.make_archive("{}_v{}".format(release_dir_copy, version), "zip", release_dir_copy)
